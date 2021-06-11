7016e00cf3a64d030154b11737be71fed6a9b0bb
Petr Balachin <pet.balachin@spyse.com>
2021-06-11 23:34:12 +0300

    feat:Added basic functionality to add and clear members

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 type Member struct {
@@ -17,22 +17,59 @@ type Members struct {
 	List []Member
 }
 
+func (ms *Members) Add(m Member) {
+	if !ms.Contatin(m) {
+		ms.List = append(ms.List, m)
+	}
+}
+
+func (ms *Members) Contatin(m Member) bool {
+	for _, el := range ms.List {
+		if el.Email == m.Email {
+			return true
+		}
+	}
+	return false
+}
+
+func (ms *Members) Clear() {
+	ms.List = []Member{}
+}
+
 func main() {
+	members := Members{List: []Member{}}
+	http.HandleFunc("/", makeSlashHandler(&members))
+	http.HandleFunc("/add", makeAddHandler(&members))
+	http.HandleFunc("/clear", makeClearHandler(&members))
 
-	data := Members{List: []Member{
-		{
-			Id:           3,
-			Name:         "Member3",
-			Email:        "mail@com.com",
-			RegisterDate: "15.04.2000",
-		},
-	}}
-
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		name := request.FormValue("Name")
-		fmt.Println(name)
-		tpl, _ := template.ParseFiles("index.html")
-		tpl.Execute(writer, data)
-	})
 	http.ListenAndServe(":8091", nil)
+}
+
+func makeSlashHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl, _ := template.ParseFiles("index.html")
+		tpl.Execute(w, members)
+	}
+}
+
+func makeAddHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		newMember := Member{
+			Id:           len(members.List) + 1,
+			Name:         r.FormValue("Name"),
+			Email:        r.FormValue("Email"),
+			RegisterDate: time.Now().Format("02.01.2006"),
+		}
+		members.Add(newMember)
+		tpl, _ := template.ParseFiles("index.html")
+		tpl.Execute(w, members)
+	}
+}
+
+func makeClearHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		members.Clear()
+		tpl, _ := template.ParseFiles("index.html")
+		tpl.Execute(w, members)
+	}
 }
