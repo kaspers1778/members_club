665d257ce156f2d70251aa6941347895a0cc5563
Petr Balachin <pet.balachin@spyse.com>
2021-06-11 22:18:54 +0300

    feat:Added template interaction

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
+type Member struct {
+	Id           int
+	Name         string
+	Email        string
+	RegisterDate string
+}
+
+type Members struct {
+	List []Member
+}
+
 func main() {
-	tpl := template.Must(template.ParseFiles("index.html"))
+
+	data := Members{List: []Member{
+		{
+			Id:           3,
+			Name:         "Member3",
+			Email:        "mail@com.com",
+			RegisterDate: "15.04.2000",
+		},
+	}}
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		tpl.Execute(writer, nil)
+		name := request.FormValue("Name")
+		fmt.Println(name)
+		tpl, _ := template.ParseFiles("index.html")
+		tpl.Execute(writer, data)
 	})
 	http.ListenAndServe(":8091", nil)
 }
