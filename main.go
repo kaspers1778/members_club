8517e7050348b82e823f4758314fe807dcec4c83
Petr Balachin <pet.balachin@spyse.com>
2021-06-12 14:24:04 +0300

    feat:Added error handling

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"net/http"
+	"regexp"
 	"time"
 )
 
@@ -13,18 +15,54 @@ type Member struct {
 	RegisterDate string
 }
 
-type Members struct {
-	List []Member
+func (m Member) Validate() (nameErr, emailErr error) {
+	isNameValid, _ := regexp.MatchString("[\\w\\.\\s]+", m.Name)
+	if !isNameValid {
+		nameErr = errors.New("Name should be valid")
+	}
+	isEmailValid, _ := regexp.MatchString("[\\w\\.\\d]+@[\\w\\d]+\\.[\\w]+", m.Email)
+	if !isEmailValid {
+		emailErr = errors.New("Email should be valid")
+	}
+	return nameErr, emailErr
+
 }
 
-func (ms *Members) Add(m Member) {
-	if !ms.Contatin(m) {
-		ms.List = append(ms.List, m)
+type Message struct {
+	Members []Member
+	Errors  map[string]string
+}
+
+func (ms *Message) ClearErrors() {
+	ms.Errors = map[string]string{}
+}
+
+func (ms *Message) ValidateMember(m Member) bool {
+	nameErr, emailErr := m.Validate()
+	if nameErr != nil {
+		ms.Errors["Name"] = nameErr.Error()
+	}
+	if emailErr != nil {
+		ms.Errors["Email"] = emailErr.Error()
+	}
+
+	if nameErr == nil && emailErr == nil {
+		return true
+	}
+	return false
+}
+
+func (ms *Message) Add(m Member) {
+	isMemberValid := ms.ValidateMember(m)
+	if ms.Contain(m) {
+		ms.Errors["Email"] = "Members club already has member with such email"
+	} else if isMemberValid {
+		ms.Members = append(ms.Members, m)
 	}
 }
 
-func (ms *Members) Contatin(m Member) bool {
-	for _, el := range ms.List {
+func (ms *Message) Contain(m Member) bool {
+	for _, el := range ms.Members {
 		if el.Email == m.Email {
 			return true
 		}
@@ -32,44 +70,45 @@ func (ms *Members) Contatin(m Member) bool {
 	return false
 }
 
-func (ms *Members) Clear() {
-	ms.List = []Member{}
+func (ms *Message) Clear() {
+	ms.Members = []Member{}
 }
 
 func main() {
-	members := Members{List: []Member{}}
-	http.HandleFunc("/", makeSlashHandler(&members))
-	http.HandleFunc("/add", makeAddHandler(&members))
-	http.HandleFunc("/clear", makeClearHandler(&members))
+	message := Message{Members: []Member{},
+		Errors: map[string]string{}}
+	http.HandleFunc("/", makeSlashHandler(&message))
+	http.HandleFunc("/add", makeAddHandler(&message))
+	http.HandleFunc("/clear", makeClearHandler(&message))
 
 	http.ListenAndServe(":8091", nil)
 }
 
-func makeSlashHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+func makeSlashHandler(members *Message) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl, _ := template.ParseFiles("index.html")
 		tpl.Execute(w, members)
 	}
 }
 
-func makeAddHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+func makeAddHandler(message *Message) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		message.ClearErrors()
 		newMember := Member{
-			Id:           len(members.List) + 1,
+			Id:           len(message.Members) + 1,
 			Name:         r.FormValue("Name"),
 			Email:        r.FormValue("Email"),
 			RegisterDate: time.Now().Format("02.01.2006"),
 		}
-		members.Add(newMember)
-		tpl, _ := template.ParseFiles("index.html")
-		tpl.Execute(w, members)
+		message.Add(newMember)
+		http.Redirect(w, r, "", http.StatusSeeOther)
+
 	}
 }
 
-func makeClearHandler(members *Members) func(w http.ResponseWriter, r *http.Request) {
+func makeClearHandler(members *Message) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		members.Clear()
-		tpl, _ := template.ParseFiles("index.html")
-		tpl.Execute(w, members)
+		http.Redirect(w, r, "", http.StatusSeeOther)
 	}
 }
